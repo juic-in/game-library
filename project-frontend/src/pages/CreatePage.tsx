5a1c946f1584e0b5fef9ed12097fb8b8d4094358
Justin Tran <justintn28@gmail.com>
2025-04-30 22:50:49 +1000

    feat: file management for pages

---
+++ project-frontend/src/pages/CreatePage.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export const CreatePage = () => {
+  const [newGame, setNewGame ] = useState({
+    name: ""
+  })
+
+  return (
+    <div>CreatePage</div>
+  )
+}
