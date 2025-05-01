28118907c17db35810ea9c3c316cb81aca1f7c0e
Justin Tran <justintn28@gmail.com>
2025-05-01 21:00:03 +1000

    feat: added controllers and set up 9 new user related routes

--- project-backend/src/controllers/user/user.controller.ts
+++ project-backend/src/controllers/user/user.controller.ts
@@ -1,10 +1,145 @@
 import { Request, Response } from 'express';
+import {
+  userAddFriend,
+  userAddGameToOwnedGames,
+  userAddGameToWishlist,
+  userGetFriends,
+  userGetOwnedGames,
+  userGetWishlist,
+  userRemoveFriend,
+  userRemoveGameFromOwnedGames,
+  userRemoveGameFromWishlist,
+} from './users';
+import { AuthenticatedRequest } from '../../middleware/authMiddleware';
 
-
-export const addToUserLib = async (req: Request, res: Response) => {
+export const addToUserLib = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
   try {
-    // await userAddGame
+    const { gameId } = req.params;
+    const result = await userAddGameToOwnedGames(req.user._id, gameId);
+    res.status(200).json({ success: true, data: result });
   } catch (error) {
-    res.status(error.statusCode || 500).json({ success: false, error: error.message })
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
   }
-}
+};
+
+export const removeFromUserLib = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const { gameId } = req.params;
+    const result = await userRemoveGameFromOwnedGames(req.user._id, gameId);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const addToUserWishlist = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const { gameId } = req.params;
+    const result = await userAddGameToWishlist(req.user._id, gameId);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const removeFromUserWishlist = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const { gameId } = req.params;
+    const result = await userRemoveGameFromWishlist(req.user._id, gameId);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const addToUserFriends = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const { gameId } = req.params;
+    const result = await userAddFriend(req.user._id, gameId);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const removeFromUserFriends = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const { gameId } = req.params;
+    const result = await userRemoveFriend(req.user._id, gameId);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const getUserGames = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const result = await userGetOwnedGames(req.user._id);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const getUserWishlist = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const result = await userGetWishlist(req.user._id);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
+
+export const getUserFriends = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const result = await userGetFriends(req.user._id);
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message });
+  }
+};
--- project-backend/src/routes/user.route.ts
+++ project-backend/src/routes/user.route.ts
@@ -1,9 +1,37 @@
 import express from 'express';
 import { requireUserAuth } from '../middleware/authMiddleware';
+import {
+  addToUserFriends,
+  addToUserLib,
+  addToUserWishlist,
+  getUserFriends,
+  getUserGames,
+  getUserWishlist,
+  removeFromUserFriends,
+  removeFromUserLib,
+  removeFromUserWishlist,
+} from '../controllers/user/user.controller';
 
 const router = express.Router();
-router.use(requireUserAuth)
 
-// router.put('/:gameId', addToUserLib);
-// 
+router.use(requireUserAuth);
+
+router.post('/:gameId/lib', addToUserLib);
+
+router.delete('/:gameId/lib', removeFromUserLib);
+
+router.post('/:gameId/wishlist', addToUserWishlist);
+
+router.delete('/:gameId/wishlist', removeFromUserWishlist);
+
+router.post('/:gameId/friends', addToUserFriends);
+
+router.delete('/:gameId/friends', removeFromUserFriends);
+
+router.get('/lib', getUserGames);
+
+router.get('/wishlist', getUserWishlist);
+
+router.get('/friends', getUserFriends);
+
 export default router;
