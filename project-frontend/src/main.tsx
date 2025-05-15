b4afe57e8745369997a79bc3370a261f8d861e23
Justin Tran <justintn28@gmail.com>
2025-05-15 17:42:22 +1000

    feat: wrap app in auth provider - context

--- project-frontend/src/main.tsx
+++ project-frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './AuthProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
