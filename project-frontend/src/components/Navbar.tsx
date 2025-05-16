054659b3851751b8f9588f1ed863bc685388db4d
Justin Tran <justintn28@gmail.com>
2025-05-16 19:27:05 +1000

    feat: conditional rendering  for register, login tabs

--- project-frontend/src/components/Navbar.tsx
+++ project-frontend/src/components/Navbar.tsx
@@ -1,27 +1,23 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
-import {
-  Box,
-  Container,
-  Flex,
-  Text,
-} from '@chakra-ui/react';
+import { Box, Container, Flex, Text } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthProvider';
 
 interface Props {
   toggleSidebar: () => void;
 }
 
-
 export const Navbar = ({ toggleSidebar }: Props) => {
+  const { isAuthenticated } = useAuth();
   useEffect(() => {
-      // Header height + Gradient bar height
-      document.body.style.marginTop = '65px';
-  
-      return () => {
-        document.body.style.marginTop = '0';
-      };
-    }, []);
+    // Header height + Gradient bar height
+    document.body.style.marginTop = '65px';
+
+    return () => {
+      document.body.style.marginTop = '0';
+    };
+  }, []);
 
   return (
     <>
@@ -52,11 +48,14 @@ export const Navbar = ({ toggleSidebar }: Props) => {
             userSelect={'none'}
             onContextMenu={(e) => e.preventDefault()}
           >
-            <HamburgerIcon color='white' w={'60px'} />
+            <HamburgerIcon color="white" w={'60px'} />
           </Box>
 
           {/* Logo (centered) */}
           <Text
+            position={'absolute'}
+            left={'50%'}
+            transform={'translateX(-50%)'}
             fontSize={{ base: '22', sm: '28' }}
             fontWeight={'bold'}
             textTransform={'uppercase'}
@@ -70,8 +69,24 @@ export const Navbar = ({ toggleSidebar }: Props) => {
           >
             <Link to={'/'}>GameXUnify</Link>
           </Text>
-
-          {/* <Box w="40px" /> */}
+          {!isAuthenticated && (
+            <Flex alignItems={'center'} justifyContent={'center'}>
+              <Text
+                fontSize={{ base: '16', sm: '20' }}
+                textTransform={'uppercase'}
+                textAlign={'center'}
+                userSelect={'none'}
+                color="white"
+                mr={5}
+              >
+                <Link to={'/auth?mode=register'}>Register</Link>
+                <Text as="span" mx={2} color="gray.500">
+                  |
+                </Text>
+                <Link to={'/auth?mode=login'}>Login</Link>
+              </Text>
+            </Flex>
+          )}
         </Flex>
         <Box
           height="3px"
